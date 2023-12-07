d9e02c7cf35da1c945c0fbe15901a148e0ef6462
C0lgate <k0lesan2k00@gmail.com>
2023-12-07 20:28:21 +0300

    fix: correct implementation burger-menu

--- js/common/burger-menu.js
+++ js/common/burger-menu.js
@@ -1,58 +1,78 @@
-if (window.innerWidth <= 768) {
-  const burgerMenu = document.querySelector('#burger-menu'),
-    burgerMenuLines = document.querySelectorAll('.burger-menu__wrapper'),
-    nav = document.querySelector('.nav'),
-    headerMenu = document.querySelector('.header-menu'),
-    headerWrapper = document.createElement('div');
+const header = document.querySelector('.header'),
+  burgerMenu = document.querySelector('#burger-menu'),
+  headerWrapper = document.querySelector('.header__wrapper'),
+  burgerMenuLines = document.querySelectorAll('.burger-menu__wrapper');
 
-  headerWrapper.append(nav, headerMenu);
-  headerWrapper.classList.add('header__wrapper');
-  burgerMenu.before(headerWrapper);
+let isClicked = false;
 
-  let isClicked = false;
-
-  function toggleMenu() {
-    if (isClicked) {
-      return;
-    }
-
-    isClicked = true;
-
-    setTimeout(() => {
-      isClicked = false;
-
-      headerWrapper.classList.add('header__wrapper_scroll');
-
-      if (
-        !burgerMenuLines[0].classList.contains('burger-menu__wrapper_active')
-      ) {
-        document.body.classList.remove('body_block-scroll');
-      }
-    }, 550);
-
-    headerWrapper.classList.remove('header__wrapper_scroll');
-    headerWrapper.classList.toggle('header__wrapper_visible');
-
-    document.body.classList.add('body_block-scroll');
-
-    burgerMenuLines.forEach((element) => {
-      element.classList.toggle('burger-menu__wrapper_active');
-    });
+function hideBurgerMenu() {
+  if (window.innerWidth <= 768) {
+    return;
   }
 
-  function scrollToSection(event) {
-    if (!event.target.href) {
-      return;
-    }
+  burgerMenuLines.forEach((element) => {
+    element.classList.remove('burger-menu__wrapper_active');
+  });
 
-    event.preventDefault();
-    toggleMenu();
-
-    setTimeout(() => {
-      window.location.href = event.target.href;
-    }, 550);
-  }
-
-  burgerMenu.addEventListener('click', toggleMenu);
-  nav.addEventListener('click', scrollToSection);
+  document.body.classList.remove('body_block-scroll');
+  headerWrapper.classList.remove('header__wrapper_visible');
+  headerWrapper.classList.remove('header__wrapper_scroll');
 }
+
+function toggleMenu() {
+  if (isClicked) {
+    return;
+  }
+
+  window.scrollTo(0, 0);
+
+  if (!headerWrapper.classList.contains('header__wrapper_visible')) {
+    headerWrapper.scrollTo(0, 0);
+  }
+
+  isClicked = true;
+
+  setTimeout(() => {
+    isClicked = false;
+
+    headerWrapper.classList.add('header__wrapper_scroll');
+
+    if (!headerWrapper.classList.contains('header__wrapper_visible')) {
+      document.body.classList.remove('body_block-scroll');
+    }
+  }, 550);
+
+  headerWrapper.classList.remove('header__wrapper_scroll');
+  headerWrapper.classList.toggle('header__wrapper_visible');
+
+  document.body.classList.add('body_block-scroll');
+
+  burgerMenuLines.forEach((element) => {
+    element.classList.toggle('burger-menu__wrapper_active');
+  });
+}
+
+function followingALink(event) {
+  const element = event.target.closest('a'),
+    delay = headerWrapper.classList.contains('header__wrapper_visible')
+      ? 550
+      : 0;
+
+  event.preventDefault();
+
+  if (element && headerWrapper.classList.contains('header__wrapper_visible')) {
+    toggleMenu();
+  }
+
+  if (element.href.at(-1) === '#') {
+    return;
+  }
+
+  setTimeout(() => {
+    window.location.href = element.href;
+  }, delay);
+}
+
+window.addEventListener('resize', hideBurgerMenu);
+header.addEventListener('click', followingALink);
+burgerMenu.addEventListener('click', toggleMenu);
