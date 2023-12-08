0059d82bf79503da765ca60953e42f654c906955
C0lgate <k0lesan2k00@gmail.com>
2023-12-08 20:39:43 +0300

    feat: implement scroll for touch device

--- js/index/slider.js
+++ js/index/slider.js
@@ -1,56 +1,101 @@
 const slider = document.querySelector('.slider'),
-  [leftArrow, rightArrow] = document.querySelectorAll('.slider__button'),
   sliderControls = document.querySelector('.slider-controls'),
   favoriteCoffeeBlock = document.querySelector(
     '.favorite-coffee-info__wrapper'
-  );
+  ),
+  arrows = document.querySelectorAll('.slider__button');
 
-let control = document.querySelector('.slider-controls__item');
+let control = document.querySelector('.slider-controls__item'),
+  durationTransition = 5,
+  startClickPosition = null;
 
-control.style.setProperty('--animate', '100%');
-
-function chandeDirection(line, coordinate, direction) {
-  return direction === 'left'
-    ? [
-        line.previousElementSibling ? coordinate + 100 : -200,
-        line.previousElementSibling || sliderControls.lastElementChild,
-      ]
-    : [
-        line.nextElementSibling ? coordinate - 100 : 0,
-        line.nextElementSibling || sliderControls.firstElementChild,
-      ];
+function setTransition(translate, transition) {
+  control.style.setProperty('--duration', transition);
+  control.style.setProperty('--animate', translate);
 }
 
 function nextCoffee(event) {
-  if (event.type === 'transitionend' && event.target !== control) {
+  const arrow = event.target.closest('.slider__button');
+
+  if (
+    (event.type === 'click' && !arrow) ||
+    (event.type === 'transitionend' && event.target !== control)
+  ) {
     return;
   }
 
-  let arrow = event.target;
+  arrows.forEach((element) => {
+    element.disabled = true;
+  });
 
-  if (event.type === 'click') {
-    arrow = arrow.closest('.slider__button') || arrow;
+  setTransition('0', 'transform ease 1s');
+
+  if (arrow?.getAttribute('data-direction') === 'left') {
+    control = control.previousElementSibling || sliderControls.lastElementChild;
+  } else {
+    control = control.nextElementSibling || sliderControls.firstElementChild;
   }
 
-  control.style.setProperty('--duration', '0.5s');
-  control.style.setProperty('--animate', '0');
+  favoriteCoffeeBlock.style.transform = `translateX(${
+    -100 * control.getAttribute('data-position')
+  }%)`;
 
-  let xCoordinate = +favoriteCoffeeBlock.style.transform.match(/-\d+/g);
-
-  [xCoordinate, control] = chandeDirection(
-    control,
-    xCoordinate,
-    arrow.getAttribute('data-direction')
-  );
-
-  favoriteCoffeeBlock.style.transform = `translateX(${xCoordinate}%)`;
+  durationTransition = 5;
 
   setTimeout(() => {
-    control.style.setProperty('--duration', '5s');
-    control.style.setProperty('--animate', '100%');
-  }, 500);
+    arrows.forEach((element) => {
+      element.disabled = false;
+    });
+
+    setTransition('100%', `transform ease 5s`);
+  }, 1000);
 }
 
-leftArrow.addEventListener('click', nextCoffee);
-rightArrow.addEventListener('click', nextCoffee);
+function setDurationTransition(event) {
+  durationTransition = Math.max(0, durationTransition - event.elapsedTime);
+}
+
+function stopTransition(event) {
+  const xCoordinate = getComputedStyle(control, '::after')
+    .transform.split(', ')
+    .at(-2);
+
+  if (['touchstart', 'mouseover'].includes(event.type)) {
+    setTransition(`${xCoordinate}px`, 'none');
+  }
+
+  if (['touchcancel', 'mouseout'].includes(event.type)) {
+    setTransition('100%', `transform ease ${durationTransition}s`);
+  }
+}
+
+function getStartClick(event) {
+  startClickPosition = event.screenX;
+}
+
+function scrollCoffee(event) {
+  const betweenDistance = startClickPosition - event.screenX;
+
+  if (Math.abs(betweenDistance) < 100) {
+    return;
+  }
+
+  arrows[Number(betweenDistance >= 0)].click();
+
+  startClickPosition = 0;
+}
+
+control.style.setProperty('--animate', '100%');
+
+sliderControls.addEventListener('transitioncancel', setDurationTransition);
+
+favoriteCoffeeBlock.addEventListener('touchstart', stopTransition);
+favoriteCoffeeBlock.addEventListener('mouseover', stopTransition);
+favoriteCoffeeBlock.addEventListener('touchcancel', stopTransition);
+favoriteCoffeeBlock.addEventListener('mouseout', stopTransition);
+
+favoriteCoffeeBlock.addEventListener('mousedown', getStartClick);
+favoriteCoffeeBlock.addEventListener('mouseup', scrollCoffee);
+
+slider.addEventListener('click', nextCoffee);
 sliderControls.addEventListener('transitionend', nextCoffee);
